85ebd0b820bc2bbccd5292d090da71ec1a9e2e4b
ark948 <armankarimi948@gmail.com>
2024-10-25 16:04:42 +0330

    db and alembic

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from api import users, sections, courses
+from db.db_setup import engine
+from db.models import user, course
 
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.creae_all(bind=engine)
 
 app = FastAPI(
     title="FastAPI LMS",
